a23c945f9b1fca1924a80e37d90c2c4e5ecb22e9
Madvith-d <madroux930@gmail.com>
2024-09-28 12:22:36 +0530

    mycommit1

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "madvith-d",
+    name: "Madvith D Amin",
+    quote: "Hey there",
+  },
 ];
